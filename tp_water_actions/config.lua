5f8288ccf2c9879efb07bc70fcfd51d9e356c1ac
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2026-03-11 01:22:30 +0200

    Update config.lua

--- tp_water_actions/config.lua
+++ tp_water_actions/config.lua
@@ -26,7 +26,7 @@ Config.PromptKeys = {
 ]]---------------------------------------------------------
 
 -- How much thirst should be added on the player when drinking from water?
--- Checkout client/main.lua (157 line) to execute your metabolism event or export.
+-- Checkout client/main.lua (154 line) to execute your metabolism event or export.
 Config.AddDrinkThirstValue = 10
 
 -- When drinking from a water source.
